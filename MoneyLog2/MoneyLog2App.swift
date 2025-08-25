35d0ff8aae7e4cd5aa36f64d448ba13f668228ac
Seoincam <seoin4715@naver.com>
2025-08-25 14:49:53 +0900

    Initial Commit

---
+++ MoneyLog2/MoneyLog2App.swift
@@ -0,0 +1,32 @@
+//
+//  MoneyLog2App.swift
+//  MoneyLog2
+//
+//  Created by 박서인 on 8/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MoneyLog2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
